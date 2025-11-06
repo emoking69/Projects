bf6499acc0e11d1fdb4cc7abac59dc3c2dfb918e
Sokolov Daniel <sokolovdaniel42@gmail.com>
2025-11-06 15:45:38 +0600

    tic tac toe

--- index.js
+++ index.js
@@ -1,34 +1,70 @@
-let current = 50;
-let left = 0;
-let right = 100;
-let found = false;
+const board = document.getElementById('board')
+const squares = document.getElementsByClassName('square')
+const players = ['X', 'O']
+let currentPlayer = players[0]
+const endMessage = document.createElement('h2')
+endMessage.textContent = `X's turn!`
+endMessage.style.marginTop = '30px'
+endMessage.style.textAlign='center'
+board.after(endMessage)
 
+const winning_combinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
 
-function Correct() {
-  alert("Yay! I guessed your number!");
-  found = true;
+for(let i = 0; i < squares.length; i++){
+    squares[i].addEventListener('click', () => {
+        if(squares[i].textContent !== ''){
+            return
+        }
+        squares[i].textContent = currentPlayer
+        if(checkWin(currentPlayer)) {
+            endMessage.textContent=`Game over! ${currentPlayer} wins!`
+            return
+        }
+        if(checkTie()) {
+            endMessage.textContent= `Game is tied!`
+            return
+        }
+        currentPlayer = (currentPlayer === players[0]) ? players[1] : players[0] 
+        if(currentPlayer == players[0]) {
+            endMessage.textContent= `X's turn!`
+        } else {
+            endMessage.textContent= `O's turn!`
+        }     
+    })   
 }
 
-function Lower() {
-  if(found != true){  
-  right = current;
-  current = (left + current) / 2;
-  document.getElementById("num").textContent = Math.trunc(current);   
-  }
+function checkWin(currentPlayer) {
+    for(let i = 0; i < winning_combinations.length; i++){
+        const [a, b, c] = winning_combinations[i]
+        if(squares[a].textContent === currentPlayer && squares[b].textContent === currentPlayer && squares[c].textContent === currentPlayer){
+            return true
+        }
+    }
+    return false
 }
 
-function Higher() {
-  if(found != true){  
-  left = current;
-  current = (current + right) / 2;
-  document.getElementById("num").textContent = Math.trunc(current);    
-  }
+function checkTie(){
+    for(let i = 0; i < squares.length; i++) {
+        if(squares[i].textContent === '') {
+            return false;
+        }
+    }
+    return true
 }
 
-function Restart() {
-    current = 50;
-    left = 0;
-    right = 100;
-    found = false;
-    document.getElementById("num").textContent = current; 
+function restartButton() {
+    for(let i = 0; i < squares.length; i++) {
+        squares[i].textContent = ""
+    }
+    endMessage.textContent=`X's turn!`
+    currentPlayer = players[0]
 }
